d8f62c37707b313826d465537b95c6ad37a839d8
Phuong vy <vy002359@gmail.com>
2023-09-19 14:05:04 +0700

    TabHocSinhForm

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ namespace ManagerStudent
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new LoginForm());
+            Application.Run(new MainForm());
         }
     }
 }
